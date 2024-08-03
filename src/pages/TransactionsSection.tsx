acf7983283e07ad8db9cfd7d3ab64eeb419cbadc
AricRedemption <aricredemption@gmail.com>
2024-08-03 23:42:18 +0800

    chore: Transation UI

--- src/pages/TransactionsSection.tsx
+++ src/pages/TransactionsSection.tsx
@@ -1,34 +1,24 @@
-
-<<<<<<< HEAD
-=======
-import { Section } from "../components/Section";
-import WalletBar from "../components/WalletConnect";
-import Header from "../layout/Header";
->>>>>>> main
-
-export default function () {
+export default function Transaction() {
   return (
-    <>
-<<<<<<< HEAD
-        <span>New transaction</span>
-        <div>
-            <img src="/../../public/assets/images/new-transaction.svg"/>
-        </div>
-        <div className="container center">
-            <span className="">Manage Assets</span>
-            <span>Send Token</span>
-            <span>Swap Tokens</span>
-
-        </div>
-=======
-
-      <Section>
-        <p className="mb-4 mt-10 text-5xl font-bold text-white">
-          THis is
-          <span className="text-electric-green">Transaction Section</span>Page
-        </p>
-      </Section>
->>>>>>> main
-    </>
+    <div className="mx-auto flex h-[450px] w-[1200px] items-center justify-between rounded-3xl bg-[#101111] pl-[56px] pr-[132px] text-white">
+      <div className="flex flex-col items-center gap-y-1">
+        <img
+          src="/../../public/assets/images/new-transaction.svg"
+          className="w-64"
+        />
+        <span className="text-2xl">New transaction</span>
+      </div>
+      <div className="flex flex-col items-center gap-y-6 text-black">
+        <button className="w-[380px] rounded-2xl bg-[#12FF80] py-6">
+          Manage Assets
+        </button>
+        <button className="w-[380px] rounded-2xl bg-[#12FF80] py-6">
+          Send Token
+        </button>
+        <button className="w-[380px] rounded-2xl bg-[#12FF80] py-6">
+          Swap Tokens
+        </button>
+      </div>
+    </div>
   );
 }
