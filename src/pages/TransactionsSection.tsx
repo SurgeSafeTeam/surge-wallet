7f4749a41eae2d213d2ee96f5d551788c4e69319
kevinsheeran <kevinsheeranxyj@gmail.com>
2024-08-03 23:15:12 +0800

    UPDATE: New transaction

---
+++ src/pages/TransactionsSection.tsx
@@ -0,0 +1,18 @@
+
+
+export default function () {
+  return (
+    <>
+        <span>New transaction</span>
+        <div>
+            <img src="/../../public/assets/images/new-transaction.svg"/>
+        </div>
+        <div className="container center">
+            <span className="">Manage Assets</span>
+            <span>Send Token</span>
+            <span>Swap Tokens</span>
+
+        </div>
+    </>
+  );
+}
